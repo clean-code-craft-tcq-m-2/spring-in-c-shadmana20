e9296465bf74ba9bcab1eda3015c7cd09daeedf9
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2022-10-15 20:14:26 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -15,5 +15,5 @@ float GetMin(const float* numberset,int setlength);
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
 
-extern int emailAlertCallCount;
-extern int ledAlertCallCount;
+//extern int emailAlertCallCount;
+//extern int ledAlertCallCount;
