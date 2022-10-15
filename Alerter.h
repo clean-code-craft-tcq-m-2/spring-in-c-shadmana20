ab05cc12c3ba6d3af1af9eb8ce32960227712f3e
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2022-10-15 19:52:47 +0530

    Update Alerter.h

--- Alerter.h
+++ Alerter.h
@@ -2,5 +2,4 @@
 typedef void (*alerter_funcptr)();
 void emailAlerter(void);
 void ledAlerter(void);
-alerter_funcptr alerters[] = {emailAlerter, ledAlerter};
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
