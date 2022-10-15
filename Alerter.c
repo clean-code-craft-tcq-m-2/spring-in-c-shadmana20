44e991edbc3155d0043bdd9989e575e2314f36d6
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2022-10-15 19:49:34 +0530

    Create Alerter.c
    Added as instructed

---
+++ Alerter.c
@@ -0,0 +1,22 @@
+#include "Alerter.h"
+void emailAlerter(void)
+{
+    int emailAlertCallCount = 0;
+
+}
+void ledAlerter(void)
+{
+    int ledAlertCallCount = 0;
+}
+
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
+{
+
+if(computedStats.max > maxThreshold){
+emailAlerter();
+emailAlertCallCount++;
+ledAlerter();
+ledAlertCallCount++;
+}
+    
+}
