3acd0cf5b0904e8399f47044014ccce876bc98eb
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2022-10-15 20:10:28 +0530

    Update Alerter.c

--- Alerter.c
+++ Alerter.c
@@ -3,12 +3,12 @@
 alerter_funcptr alerters[] = {emailAlerter, ledAlerter};
 void emailAlerter(void)
 {
-    int emailAlertCallCount = 0;
+   emailAlertCallCount = 0;
 
 }
 void ledAlerter(void)
 {
-    int ledAlertCallCount = 0;
+    ledAlertCallCount = 0;
 }
 
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
