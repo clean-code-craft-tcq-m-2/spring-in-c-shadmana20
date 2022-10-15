2ab0f7819fd00244055732fee845bc24d4a10e68
shadmana20 <115541042+shadmana20@users.noreply.github.com>
2022-10-15 20:05:34 +0530

    Update Alerter.h

--- Alerter.h
+++ Alerter.h
@@ -1,7 +1,7 @@
-#include "stats.h"
+//#include "stats.h"
 typedef void (*alerter_funcptr)();
 void emailAlerter(void);
 void ledAlerter(void);
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
+//void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
 int emailAlertCallCount;
 int ledAlertCallCount;
